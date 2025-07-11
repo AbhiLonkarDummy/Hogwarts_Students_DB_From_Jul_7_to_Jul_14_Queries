83b77971e435a3137d73acf09caf1abd49e82479
Abhishek Lonkar <abhilonkardummy@outlook.com>
2025-07-11 16:23:30 +0530

    Update Create_Table.sql

--- Create_Table.sql
+++ Create_Table.sql
@@ -1,24 +1,24 @@
--- CREATE TABLE students (
---     student_id INT PRIMARY KEY AUTO_INCREMENT,
---     first_name VARCHAR(50) NOT NULL,
---     last_name VARCHAR(50) NOT NULL,
---     date_of_birth DATE NOT NULL,
---     gender ENUM('Male', 'Female') NOT NULL,
---     house ENUM('Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin') NOT NULL,
---     pet_name VARCHAR(50),
---     quidditch_team BOOLEAN DEFAULT FALSE
--- );
+CREATE TABLE students (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    gender ENUM('Male', 'Female') NOT NULL,
+    house ENUM('Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin') NOT NULL,
+    pet_name VARCHAR(50),
+    quidditch_team BOOLEAN DEFAULT FALSE
+);
 
--- CREATE TABLE courses (
---     course_id INT AUTO_INCREMENT PRIMARY KEY,
---     course_name VARCHAR(100) NOT NULL,
---     professor VARCHAR(100) NOT NULL
--- );
+CREATE TABLE courses (
+    course_id INT AUTO_INCREMENT PRIMARY KEY,
+    course_name VARCHAR(100) NOT NULL,
+    professor VARCHAR(100) NOT NULL
+);
 
--- CREATE TABLE enrollments (
---     enrollment_id INT AUTO_INCREMENT PRIMARY KEY,
---     student_id INT NOT NULL,
---     course_id INT NOT NULL,
---     FOREIGN KEY (student_id) REFERENCES students(student_id),
---     FOREIGN KEY (course_id) REFERENCES courses(course_id)
--- );
+CREATE TABLE enrollments (
+    enrollment_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT NOT NULL,
+    course_id INT NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
