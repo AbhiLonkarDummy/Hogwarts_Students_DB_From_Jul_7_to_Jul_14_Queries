313620ad097c65b4b5cec0989877bee3f8efb0b5
Abhishek Lonkar <abhilonkardummy@outlook.com>
2025-07-11 16:23:08 +0530

    Update Selecting.sql

--- Selecting.sql
+++ Selecting.sql
@@ -1,6 +1,6 @@
 -- Display the entire students table
--- SELECT * FROM students;
--- SELECT * FROM courses;
+SELECT * FROM students;
+SELECT * FROM courses;
 
 -- Fetching students who are older than 18 and then sort them by age
 -- Now the problem over here is that all of the records of this table are over the age of 18 if we consider currYear as 2025
@@ -8,9 +8,9 @@
 -- We can use the date diff function
 
 -- Date diff looks a little dirty needed to use floor as well 
--- SELECT student_id, first_name, last_name, gender, house, FLOOR(DATEDIFF('1995-01-01', date_of_birth) / 365.25) AS age FROM students
--- WHERE DATEDIFF('1995-01-01', date_of_birth)/365 >= 18 ORDER BY age DESC;
+SELECT student_id, first_name, last_name, gender, house, FLOOR(DATEDIFF('1995-01-01', date_of_birth) / 365) AS age FROM students
+WHERE DATEDIFF('1995-01-01', date_of_birth)/365 >= 18 ORDER BY age DESC;
 
 -- Time stamp better 
--- SELECT student_id, first_name, last_name, gender, house, TIMESTAMPDIFF(YEAR, date_of_birth, '1995-01-01') AS age FROM students
--- WHERE TIMESTAMPDIFF(YEAR, date_of_birth, '1995-01-01') >= 18 ORDER BY age DESC;
+SELECT student_id, first_name, last_name, gender, house, TIMESTAMPDIFF(YEAR, date_of_birth, '1995-01-01') AS age FROM students
+WHERE TIMESTAMPDIFF(YEAR, date_of_birth, '1995-01-01') >= 18 ORDER BY age DESC;
